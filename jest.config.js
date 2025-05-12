ed034ae4a3701d71b1bf717a181183524ec3e7af
Bill Alexander <wralex@gmail.com>
2025-05-12 08:50:19 -0400

    Updates

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,30 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 /** @type {import('jest').Config} **/
-module.exports = { // Reference site: https://jestjs.io/docs/configuration
+
+module.exports = {
+  // Reference site: https://jestjs.io/docs/configuration
   testEnvironment: "node",
   presets: [
     ['@babel/preset-env', {targets: {node: 'current'}}],
     '@babel/preset-typescript',
   ],
   transform: {
-    "^.+\.tsx?$": [ "ts-jest",{} ],
+    '^.+\.tsx?$': [ "ts-jest",{} ],
   },
   collectCoverage: true,
   coverageDirectory: "./test-results/coverage",
   collectCoverageFrom: [
     '**/src/**/*.{ts,tsx}',
     '**/src/**/*.{js,jsx}',
-    '!**/node_modules/**',
-    '!**/vendor/**',
+    '**/public/**/*.{js,jsx}',
+    '!**/node_modules/**'
   ],
-  coverageReporters: ["html", "cobertura"],
+  coverageReporters: ["html", "xml", "cobertura"],
   reporters: [
     "default",
     [
-      "jest-junit", { // Reference site: https://github.com/jest-community/jest-junit/blob/master/README.md
+      // Reference site: https://github.com/jest-community/jest-junit/blob/master/README.md
+      "jest-junit", {
         suiteName: "Jest Tests",
         outputDirectory: "./test-results/unit",
         outputName: `junit-${(new Date()).toISOString().replaceAll(':','')}.xml`,
--- public/scripts/site.js
+++ public/scripts/site.js
@@ -1,3 +1,5 @@
+const $ = require('jquery');
+
 $(()=>{
     $('.markdown-it-code-copy').on('click', (e) => {
         const text=$(e.currentTarget).data('clipboard-text'); 
@@ -6,9 +8,10 @@ $(()=>{
     });
 });
 
-function copyToClipboard(text) {
+export function copyToClipboard(text) {
     navigator.clipboard.writeText(text)
     .catch(err => {
         console.error('Failed to copy text: ', err);
     });
 }
+
